f0bb595fc9708eb8be08a213d1b54a3a30c799f0
Andrés <cdmx1674@amerike.edu.mx>
2021-06-08 20:39:57 -0500

    here i go again :,v

--- Assets/Scripts/ButtonScript.cs
+++ Assets/Scripts/ButtonScript.cs
@@ -4,19 +4,11 @@ using UnityEngine;
 
 public class ButtonScript : MonoBehaviour
 {
-    public void SyncCardAToButton(Card cardinfo)
+    public void SyncCardToButton(Card cardinfo)
 	{
 		GameObject Manager = GameObject.Find("MANAGER");		
 		var cardDisplay = Manager.GetComponent<CardDisplay>();
 		cardDisplay.card = cardinfo;
-		cardDisplay.LoadCardInfo();
-	}
-	
-	public void SyncCardBToButton(CardB cardinfo)
-	{
-		GameObject Manager = GameObject.Find("MANAGER");		
-		var cardDisplay = Manager.GetComponent<CardBDisplay>();
-		cardDisplay.cardB = cardinfo;
 		cardDisplay.LoadCardInfo();
 	}
 }
--- Assets/Scripts/SyncDisplay.cs
+++ Assets/Scripts/SyncDisplay.cs
@@ -6,7 +6,6 @@ public class SyncDisplay : MonoBehaviour
 {
     public Transform MainCamera;
 	public Transform display;
-	public Transform display2;
 	public Transform otherdisplay;
 	
 
@@ -15,24 +14,10 @@ public class SyncDisplay : MonoBehaviour
         Vector3 OffsetDisplays = MainCamera.position - otherdisplay.position;
 		//transform.position = display.position + OffsetDisplays;
 		
-		if(display.gameObject.activeSelf)
-		{
-			float diffBetweenDisplayRotations = Quaternion.Angle(display.rotation, otherdisplay.rotation);
+		float diffBetweenDisplayRotations = Quaternion.Angle(display.rotation, otherdisplay.rotation);
 		
-			Quaternion displayRotationalDiff = Quaternion.AngleAxis(diffBetweenDisplayRotations, Vector3.up);
-			Vector3 newDir = displayRotationalDiff * MainCamera.forward;
-			transform.rotation = Quaternion.LookRotation(newDir, Vector3.up);
-		}
-		
-		if(display2.gameObject.activeSelf)
-		{
-			float diffBetweenDisplayRotations = Quaternion.Angle(display2.rotation, otherdisplay.rotation);
-		
-			Quaternion displayRotationalDiff = Quaternion.AngleAxis(diffBetweenDisplayRotations, Vector3.up);
-			Vector3 newDir = displayRotationalDiff * MainCamera.forward;
-			transform.rotation = Quaternion.LookRotation(newDir, Vector3.up);
-		}
-		
-
+		Quaternion displayRotationalDiff = Quaternion.AngleAxis(diffBetweenDisplayRotations, Vector3.up);
+		Vector3 newDir = displayRotationalDiff * MainCamera.forward;
+		transform.rotation = Quaternion.LookRotation(newDir, Vector3.up);
     }
 }
