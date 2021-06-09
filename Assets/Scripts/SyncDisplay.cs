70c5bc027c70a1ddbc18c2ee13c66c2309a7db05
Andrés <cdmx1674@amerike.edu.mx>
2021-06-09 11:15:06 -0500

    Ultimo Intento :,v

--- Assets/Scripts/SyncDisplay.cs
+++ Assets/Scripts/SyncDisplay.cs
@@ -6,6 +6,7 @@ public class SyncDisplay : MonoBehaviour
 {
     public Transform MainCamera;
 	public Transform display;
+	public Transform display2;
 	public Transform otherdisplay;
 	
 
@@ -14,10 +15,24 @@ public class SyncDisplay : MonoBehaviour
         Vector3 OffsetDisplays = MainCamera.position - otherdisplay.position;
 		//transform.position = display.position + OffsetDisplays;
 		
-		float diffBetweenDisplayRotations = Quaternion.Angle(display.rotation, otherdisplay.rotation);
+		if(display.gameObject.activeSelf)
+		{
+			float diffBetweenDisplayRotations = Quaternion.Angle(display.rotation, otherdisplay.rotation);
 		
-		Quaternion displayRotationalDiff = Quaternion.AngleAxis(diffBetweenDisplayRotations, Vector3.up);
-		Vector3 newDir = displayRotationalDiff * MainCamera.forward;
-		transform.rotation = Quaternion.LookRotation(newDir, Vector3.up);
+			Quaternion displayRotationalDiff = Quaternion.AngleAxis(diffBetweenDisplayRotations, Vector3.up);
+			Vector3 newDir = displayRotationalDiff * MainCamera.forward;
+			transform.rotation = Quaternion.LookRotation(newDir, Vector3.up);
+		}
+		
+		if(display2.gameObject.activeSelf)
+		{
+			float diffBetweenDisplayRotations = Quaternion.Angle(display2.rotation, otherdisplay.rotation);
+		
+			Quaternion displayRotationalDiff = Quaternion.AngleAxis(diffBetweenDisplayRotations, Vector3.up);
+			Vector3 newDir = displayRotationalDiff * MainCamera.forward;
+			transform.rotation = Quaternion.LookRotation(newDir, Vector3.up);
+		}
+		
+
     }
 }
